4680616bceb6e3fa48dbd42a066e420ae52bd4c8
Francisco Stanfield <stanfield.francisco2021@gmail.com>
2024-09-21 16:54:24 -0400

    Basic level setup

--- mobile-gyro-survivor/random_object_placer.gd
+++ mobile-gyro-survivor/random_object_placer.gd
@@ -1,13 +1,14 @@
 extends Node
 
-@export var Item:Node3D = preload("res://Items/orb.tscn").instantiate()
+@export var Item:Node3D
 @export var item_range: float = 560.34
+@export var number_of_items:int
 func  _ready():
 	place_item()
 	pass
 
 func place_item():
-	for n in (int(item_range) * 25):
+	for n in number_of_items:
 		var new_item:Node3D = Item.duplicate()
 		add_child(new_item)
 		new_item.position.x = randf_range(item_range, -item_range)
